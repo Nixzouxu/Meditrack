941226c1c443ba052c72285ac9fd42b3c061f68a
Muhammad Hafidz <24hafidz@mhs.usk.ac.id>
2025-05-11 17:22:46 +0700

    Update meditrack.c
    Menambahkan Fungsi untuk menampilkan loading pada saat awal program serta menambahkan fungsi untuk clear screen

--- meditrack.c
+++ meditrack.c
@@ -9,3 +9,29 @@ void displayHeader() {
     printf("  MediTrack: Inventory & Expiry Analyzer  \n");
     printf("========================================\n");
 }
+
+// Fungsi untuk menampilkan efek loading
+void showLoading() {
+    // Menampilkan animasi loading dengan titik
+    printf("Memuat sistem ");
+    for (int i = 0; i < 4; i++) {
+        printf(".");
+        fflush(stdout); // Memastikan teks ditampilkan langsung
+        #ifdef _WIN32
+            Sleep(500); // Jeda 500ms di Windows
+        #else
+            usleep(500000); // Jeda 500ms di Linux/Mac
+        #endif
+    }
+    printf("\n");
+}
+
+// Fungsi untuk membersihkan layar
+void clearScreen() {
+    // Memilih perintah berdasarkan sistem operasi
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}

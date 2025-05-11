cc78d59a0f327a1c5f2cfa5ae14a8f3b8beb4244
Muhammad Hafidz <24hafidz@mhs.usk.ac.id>
2025-05-11 17:02:51 +0700

    Create meditrack.c
    Membuat header di file utama
    serta membuat fungsi untuk menampilkan header program

---
+++ meditrack.c
@@ -0,0 +1,9 @@
+// Program utama MediTrack
+// Mengimpor definisi dan deklarasi dari file header
+#include "meditrack.h"
+
+// Fungsi untuk menampilkan header program
+void displayHeader() {
+    // Menampilkan judul program
+    printf("=== MediTrack: Medicine Inventory & Expiry Analyzer ===\n");
+}
